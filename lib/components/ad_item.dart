e4a83b18637c7a62c1a013c507b4a1cf2cecac1f
Ericgacoki <gacokieric@gmail.com>
2023-12-11 12:10:36 +0300

    add tap detector on close button

--- lib/components/ad_item.dart
+++ lib/components/ad_item.dart
@@ -13,48 +13,54 @@ class AdItem extends StatelessWidget {
       width: double.maxFinite,
       margin: const EdgeInsets.all(8),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
+        borderRadius: BorderRadius.circular(20),
         image: DecorationImage(image: AssetImage(imageUrl), fit: BoxFit.cover),
       ),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Ad text
-          Padding(
-            padding: const EdgeInsets.all(12),
-            child: Container(
-              height: 24,
-              width: 24,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  color: const Color(0xFF453944).withOpacity(.8),
-                  borderRadius: const BorderRadius.all(Radius.circular(8))),
-              child: const Text("Ad",
-                  style: TextStyle(
-                      fontSize: 13,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w400)),
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Ad text
+            Padding(
+              padding: const EdgeInsets.all(12),
+              child: Container(
+                height: 24,
+                width: 24,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: const Color(0xFF453944).withOpacity(.75),
+                    borderRadius: const BorderRadius.all(Radius.circular(8))),
+                child: const Text("Ad",
+                    style: TextStyle(
+                        fontSize: 13,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400)),
+              ),
             ),
-          ),
 
-          // Cancel button (requires premium)
-          Padding(
-            padding: const EdgeInsets.all(12),
-            child: Container(
-              height: 24,
-              width: 24,
-              alignment: Alignment.center,
-              decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(24))),
-              child: Icon(
-                  Icons.cancel_rounded,
-                  color: const Color(0xFF453944).withOpacity(.8)),
+            // Cancel button (requires premium)
+            Padding(
+              padding: const EdgeInsets.all(12),
+              child: GestureDetector(
+                onTap: () {
+                  // TODO: Show bottom sheet asking to upgrade to get rid of ads!
+                },
+                child: Container(
+                  height: 24,
+                  width: 24,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: const Color(0xFF453944).withOpacity(.5),
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(24))),
+                  child: const Text(
+                    "X",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
             ),
-          ),
-        ],
-      ),
+          ]),
     );
   }
 }
