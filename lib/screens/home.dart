c6236d0fc31b582ccbf3f9bd5dc78fac4f73670b
Ericgacoki <gacokieric@gmail.com>
2023-12-11 12:11:22 +0300

    align top sheet items to the left

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,14 +13,14 @@ void main() {
 class NewsApiApp extends StatelessWidget {
   const NewsApiApp({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the root of your app.
   @override
   Widget build(BuildContext context) {
     MaterialColor customColor = createMaterialColor(const Color(0xFF453944));
 
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
+        fontFamily: 'Lato',
         primarySwatch: customColor,
       ),
       home: const MyHomePage(pageTitle: 'Top Headlines'),
@@ -71,14 +71,110 @@ class _MyHomePageState extends State<MyHomePage> {
                   context,
                   Container(
                     margin: const EdgeInsets.only(
-                        top: 50, left: 12, right: 12, bottom: 24),
+                        top: 50, left: 16, right: 16, bottom: 24),
                     alignment: Alignment.topLeft,
                     width: double.maxFinite,
                     child: Column(
-                      children: List.generate(
-                          10, (index) => (Text("Hello ${index + 1}"))),
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.arrow_back)),
+                            const SizedBox(width: 30),
+                            const Text(
+                              "Filter what you see",
+                              style: TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.w700,
+                                  fontFamily: 'Lato'),
+                            )
+                          ],
+                        ),
+                        const SizedBox(height: 8),
+                        const Text(
+                          "Source",
+                          style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: 'Lato'),
+                        ),
+                        const Text(
+                          "News categories will not be available if you select specific source(s)",
+                          style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                              fontFamily: 'Lato'),
+                        ),
 
-                      // REMEMBER: Proceed from here! [Sun Dec 10, 2023]
+                        const SizedBox(height: 150),
+                        // TODO: Add flow layout with checkable chips here
+
+                        const Text(
+                          "Country",
+                          style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: 'Lato'),
+                        ),
+                        const SizedBox(height: 8),
+                        const Text(
+                          "Sources will be ignored if you select a specific country",
+                          style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                              fontFamily: 'Lato'),
+                        ),
+
+                        // Drop down and Clear button
+                        const SizedBox(
+                          height: 12,
+                        ),
+/*
+                        GridView.builder(
+                            gridDelegate:
+                                SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                            ),
+                            itemCount: 2,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Container(
+                                  margin: EdgeInsets.all(8.0),
+                                  padding: EdgeInsets.all(16.0),
+                                  color: Colors.blue,
+                                  // Replace with your desired container color
+                                  child: Center(
+                                    child: Text(
+                                      'Container ${index + 1}',
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ));
+                            }),*/
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 200,
+                              height: 40,
+                              color: Colors.blue,
+                            ),
+                            Container(
+                              margin: const EdgeInsets.only(right: 12, left: 8),
+                              child: MaterialButton(
+                                color: const Color(0XFFF5ECF4),
+                                onPressed: () {},
+                                child: const Text(
+                                  "Clear Selection",
+                                  style: TextStyle(fontSize: 16),
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
                     ),
                   ),
                 );
@@ -114,7 +210,6 @@ class _MyHomePageState extends State<MyHomePage> {
                       if (entry.key != selectedNewsCategoryKey) {
                         // TODO: Update Headlines data to match this category
                       }
-
                       setState(() {
                         selectedNewsCategoryKey = entry.key;
                       });
@@ -126,22 +221,23 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           const NewsItem(
             isBookMarked: true,
-            imageUrl: "assets/images/test_news_image.png",
-            title: 'Shocking as Kenya is set to be sold to China!',
+            imageUrl: "assets/images/customers.jpg",
+            title:
+                'Experts raise concerns about U.S. commitment to GPS modernization',
             source: "MLB Trade Rumors",
             publishedAt: "Today",
-            authors: ["Quinn Parker", "Ethan James"],
+            author: "Quinn Parker",
           ),
-          const AdItem(imageUrl: "assets/images/wallpaper.jpg"),
+          const AdItem(imageUrl: "assets/images/phone_ad.jpg"),
           const NewsItem(
             isBookMarked: false,
             imageUrl: "assets/images/wallpaper.jpg",
-            title: 'Republican Sen Tim Scott suspends presidential campaign',
+            title: 'Crypto lawyer wants to depose Changpeng',
             source: "CNN",
             publishedAt: "2 days ago",
-            authors: ["Leia Kiara", "Ethan James", "Quinn Parker"],
+            author: "Leia Kiara",
           ),
-          const AdItem(imageUrl: "assets/images/wallpaper.jpg")
+          const AdItem(imageUrl: "assets/images/spotify_ad.png"),
         ],
       ),
     );
